6415c04be2d9500eebc57899e3a603d55effde51
DeeStands4Dee <dennydam17@gmail.com>
2026-07-19 16:59:36 +0200

    jar: aggiunto JAR e script bat per map e map-ext

---
+++ tree-ext/jar/avvia_client.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\Users\D'AMBROSIO\Downloads\openjfx-21.0.11_windows-x64_bin-sdk\javafx-sdk-21.0.11\lib" --add-modules javafx.controls,javafx.fxml -jar client.jar
+pause
---
+++ tree/jar/avvia_client.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar client.jar localhost 8080
+pause
